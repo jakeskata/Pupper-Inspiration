337ff21689ddb6337cfa00709418acbc42da3777
Jacob Murray <jakeskata@gmail.com>
2023-04-13 15:27:45 -0400

    forked repo from my branch

---
+++ README.md
@@ -0,0 +1,34 @@
+# PUPPER Inspiration
+
+## Where You Can Find Us
+https://spectralraiden.github.io/Pupper-Inspiration
+
+<img src="./assets/images/page.png" width="500" height="500"> 
+
+## User Story
+```
+AS A webpage user 
+I WANT to generate a random dog pic along with a random inspirational quote
+SO THAT I can find inspiration while browsing photos of dogs
+```
+## Acceptance Criteria
+```
+GIVEN I want to get a random dog picture and inspirational quote
+WHEN I click a button on the site
+```
+
+## Site Screenshot
+```
+```
+
+## Description
+```
+This website allows users to generate a random dog picture as well as a random quote to go along with it, because who honestly hasn't found some spark of inspiration while playing with their pupper at some time or another.
+```
+## Credits
+```
+Harrison Tran-Chung (github: htranchung)
+Jacob Murray (github: jakeskata)
+Jeremy Shimetz (github: jshimetz)
+Joshua Spencer (github: SpectralRaiden)
+```
---
+++ assets/js/script.js
@@ -0,0 +1,62 @@
+const api_url = "https://zenquotes.io/api/random/";
+const div = document.querySelector('.quote-list');
+
+async function getapi(url) {
+  const response = await fetch(url);
+  var data = await response.json();
+  console.log(data[0].q);
+
+  div.innerHTML = `<div>${data[0].q}</div>`;
+}
+
+var requestUrl = 
+'https://dog.ceo/api/breeds/image/random';
+
+var clickButton = document.getElementById('clickButton');
+var image = document.getElementById("image");
+
+clickButton.addEventListener("click", function () {
+  fetch(requestUrl)
+  .then(function (response) {
+      return response.json();
+  })
+  .then(function (data) {
+    display_image(data.message)
+    image.classList.add('randomDog');
+    getapi(api_url);
+  })
+});
+
+function display_image(img) {
+ document.getElementById("image").src = img;
+}
+
+var openModal = document.querySelector(".openModal");
+var closeModal = document.querySelector("#closeModal");
+
+openModal.addEventListener("click", function () {
+  document.querySelector(".bgmodal").style.display = 'flex';
+});
+
+closeModal.addEventListener("click", function () {
+  document.querySelector(".bgmodal").style.display = 'none';
+});
+
+
+var nameInput = document.getElementById('name');
+var emailInput = document.getElementById('email');
+var messageInput = document.getElementById('message');
+
+document.querySelector('.bgmodal').addEventListener('submit', function (event) {
+  event.preventDefault();
+  if (nameInput.value) {
+    console.log(nameInput.value);
+  }
+  if (emailInput.value) {
+    console.log(emailInput.value);
+  }
+  if (messageInput.value) {
+    console.log(messageInput.value);
+  }
+  document.getElementById('form').reset();
+});
